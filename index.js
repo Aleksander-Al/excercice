ac60b05b8ecd4dda42c0b1ca08e7bdee5d739928
Aleksander-Al <1229101@nvna.eu>
2022-02-17 08:36:27 +0200

    Upload basic js examples

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ let object = {
 }
 //console.log(object.title)
 
-for (let i = 0; i < array.length; i++) {
-    console.log((array[i]));
-}
+// for (let i = 0; i < array.length; i++) {
+//     console.log((array[i]));
+// }
 
-for (let element of array) {
-    console.log(element);
-}
+// for (let element of array) {
+//     console.log(element);
+// }
 
 // let i = 0;
 // while (i < array.length) {
@@ -39,10 +39,42 @@ for (let element of array) {
 function func() {
     console.log("Executed the function");
 }
-func();
+//func();
 
 const secondFunction = () => {
     console.log("Executed the secondFunction");
 }
+//secondFunction();
 
-secondFunction();
+//======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2======UPR 2
+
+let data = [
+    10,
+    7,
+    25,
+    10,
+    10,
+    56
+]
+
+const modifiedArray = data.map((element) => {
+    if (element === 7) {
+        element = 17
+    }
+    return element
+})
+// console.log(modifiedArray)
+
+const filteredData = data.filter((element) => {
+    if (element >= 12){
+        return element
+    }
+})
+// console.log(filteredData)
+
+const foundElement = data.find((element) => {
+    if (element === 10) {
+        return element;
+    }
+})
+// console.log(foundElement)
